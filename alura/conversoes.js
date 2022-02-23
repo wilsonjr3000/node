3ce89b5d1a995e336ca30fdc68ac1e1fb4efed00
wilsonjr3000 <wilsonjr3000@gmail.com>
2022-02-23 17:45:21 -0300

    first

---
+++ alura/conversoes.js
@@ -0,0 +1,14 @@
+var respodtaDeTudo = 42
+let idade = 29
+const pi = 3.14
+
+{
+    var respodtaDeTudo = 3.14
+    let idade = 42
+    const pi = 29
+
+    console.log(respodtaDeTudo, idade, pi)
+
+}
+
+console.log(respodtaDeTudo, idade, pi)
