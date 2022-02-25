ab86ab4a3b0a078f8c723127cc29983101e5ac65
wilsonjr3000 <wilsonjr3000@gmail.com>
2022-02-25 09:30:09 -0300

    array

---
+++ alura/array.js
@@ -0,0 +1,18 @@
+
+
+// let nota1 = 10
+// let nota2 = 7
+// let nota3 = 8
+// let nota4 = 10
+
+// let media = (nota1 + nota2 + nota3 + nota4)/4 
+
+// console.log(media)
+
+const notas = [10, 7, 8, 10]
+
+
+
+let media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length
+
+console.log(media)
